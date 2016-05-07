c80586388e233c3bf8d86f3481a0130644f3249d
Dwi Indah Cahyani <dwiindah922@gmail.com>
2016-05-07 19:41:53 +0700

    Fitur Home
    Fitur Home yang berisi Login, Sekilas Info dan terdapat navigasi bar

---
+++ home.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+<script type="text/JavaScript">
+<!--
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+//-->
+</script>
+<style>
+ul
+{
+list-style-type:none;
+margin:0;
+padding:0;
+padding-top:6px;
+padding-bottom:6px;
+}
+li
+{
+display:inline;
+}
+a:link,a:visited
+{
+font-weight:bold;
+color:#FFFFFF;
+background-color:#FF9900;
+text-align:center;
+padding:6px;
+text-decoration:none;
+text-transform:uppercase;
+}
+a:hover,a:active
+{
+background-color:#FF9900;
+}
+</style>
+</head>
+<body 
+onload="MM_preloadImages('../Unnamed Site 13/LOGO BEM NEW.png','../Site13/LOGO BEM NEW.png','../../Site13/LOGO BEM NEW.png')">
+<table width="1353" height="618" border="0" cellpadding="0" cellspacing="0">
+  <tr>
+    <td width="1353" height="10" bgcolor="#FFCC33"><div align="center"><a href="#" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('logo','','../Site13/LOGO BEM NEW.png',1)"></a><a href="#" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('logounair','','../Unnamed Site 13/LOGO BEM NEW.png',1)"></a><a href="#" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image6','','../../Site13/LOGO BEM NEW.png',1)"><img src="../../Site13/logo_unair.gif" name="Image6" width="119" height="114" border="0" id="Image6" /></a></div></td>
+  </tr>
+  <tr>
+    <td height="34" bgcolor="#FF9900"><div align="center">
+      <ul>
+        <li><a href="#home">| Home |</a></li>
+        <li><a href="#news">| Reseach |</a></li>
+        <li><a href="#contact">| Achivement |</a></li>
+        <li><a href="#contact">| Galeri |</a></li>
+        <li><a href="#about">| About |</a>
+          <table width="646" border="0" cellspacing="0" cellpadding="0">
+            <tr>
+              <th width="111" scope="col">USERNAME : </th>
+              <th width="150" scope="col"><form id="form1" name="form1" method="post" action="">
+                <label for="textfield"></label>
+                <input type="text" name="textfield" id="textfield" />
+              </form></th>
+              <th width="139" scope="col">PASSWORD : </th>
+              <th width="153" scope="col"><form id="form2" name="form2" method="post" action="">
+                <label for="label"></label>
+                <label for="label"></label>
+                <input type="text" name="textfield2" id="label" />
+              </form></th>
+              <th width="93" scope="col"><form id="form3" name="form3" method="post" action="">
+                <label for="Submit"></label>
+                <input type="submit" name="Submit" value="Submit" id="Submit" />
+              </form></th>
+            </tr>
+            </table>
+        </li>
+      </ul>
+    </div></td>
+  </tr>
+  <tr>
+    <td height="378" bgcolor="#FF9900">
+	<!--END LOGIN-->
+	
+    <!--SLIDER-->
+<table bgcolor="#FFFFFF" width="1345" height="400" border="0" cellpadding="0" cellspacing="0">
+  <tr>
+    <th align="left"><marquee behavior='scroll' class='html-marquee' direction="left" onmouseout='this.start();' onmouseover='this.stop();' scrollamount='6' scrolldelay='5'>
+
+           <!--Pemasukan Gambar-->
+<a href='#'target='_blank'><img border="0" height="400" src="1d.jpg" width="1345"/><a>
+<a href='#'target='_blank'><img border="0" height="400" src="2d.jpg" width="1345" /></a>
+<a href='#'target='_blank'><img border="0" height="400" src="3d.jpg" width="1345" /></a>
+<a href='#'target='_blank'><img border="0" height="400" src="4d.jpg" width="1345" /></a>
+                    </marquee>
+	  </table>
+	<!--END SLIDER-->	</td>
+  </tr>
+  <tr>
+    <td height="15" bgcolor="#104f94">
+	<p align="center"> copyright @CherryCode2016<p>
+	</td>
+  </tr>
+</table>
+</body>
+</html>
